b7d87fcf1485a77f132a992c9182ae6e7f6dd326
Sviatoslav <downadow@ro.ru>
2024-08-19 16:11:39 +0300

    fix

--- c2xcc.py
+++ c2xcc.py
@@ -623,7 +623,10 @@ if __name__ == '__main__':
         print('usage: ./c2xcc.py FILE [CPP_ARG] [> OUT_FILE]')
         sys.exit(1)
     
-    ast = parse_file(sys.argv[1], use_cpp=True, cpp_args=['-D__XCC_C__=1', ('' if len(sys.argv) < 3 else sys.argv[2])])
+    cppargs = ['-D__XCC_C__=1']
+    if len(sys.argv) > 2:
+        cppargs += [sys.argv[2]]
+    ast = parse_file(sys.argv[1], use_cpp=True, cpp_args=cppargs)
     
     print(get_init_code())
     for item in ast:
