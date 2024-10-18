5ca25c75f2a374bebf0816788250ea9e74a3a7ae
Sviatoslav <downadow@ro.ru>
2024-10-18 17:26:43 +0300

    upd.

--- include/stdarg.h
+++ include/stdarg.h
@@ -3,10 +3,10 @@
 
 typedef int va_list;
 
-#define va_start(l, last)    (l) = 0
+#define va_start(l, last)    ((l) = 0)
 #define va_arg(l, t)         (*((t)(&___vargs)+(l++)))
 #define va_end(l)
-#define va_copy(dst, src)    (dst) = (src)
+#define va_copy(dst, src)    ((dst) = (src))
 
 #endif
 
