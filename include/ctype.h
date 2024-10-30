a8e3a8d2c183dab63a695d9c1b95060bdf60f145
Sviatoslav <downadow@ro.ru>
2024-10-30 08:55:23 +0300

    remove toascii()

--- include/ctype.h
+++ include/ctype.h
@@ -17,8 +17,6 @@ int isspace(int c)  { return (c == ' ' || (c >= '\t' && c <= '\r')); }
 int isascii(int c)  { return (c >= 0 && c <= 127);        }
 int ispunct(int c)  { return (isprint(c) && !isalnum(c)); }
 
-int toascii(int c)  { return c & 127; }
-
 int tolower(int c)  { return (isupper(c) ? c + 32 : c);   }
 int toupper(int c)  { return (islower(c) ? c - 32 : c);   }
 
